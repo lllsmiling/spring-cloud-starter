ac5fe1d0aa69827148ca4976debf8c8462933dea
Lilonglong <lllsmiling@126.com>
2018-12-24 11:28:30 +0800

    init

---
+++ eureka-server/src/main/java/wang/lingqi/EurekaServer.java
@@ -0,0 +1,33 @@
+package wang.lingqi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+/**
+ * 描述:
+ *
+ * @outhor LiLongLong
+ * @create 2018-12-24 11:16
+ */
+@SpringBootApplication
+@EnableEurekaServer
+public class EurekaServer {
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaServer.class, args);
+    }
+
+    /**
+     * 2.1.1
+     */
+    @EnableWebSecurity
+    static class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+        @Override
+        protected void configure(HttpSecurity http) throws Exception {
+            http.csrf().disable();
+        }
+    }
+}
